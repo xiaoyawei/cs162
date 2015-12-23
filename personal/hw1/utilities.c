ba12dc475bcf606a918eae58b92c7185fa4b9069
xwei <xiaoya2wei@gmail.com>
2015-12-23 06:39:12 +0000

    input / output redirection

--- personal/hw1/utilities.c
+++ personal/hw1/utilities.c
@@ -2,10 +2,13 @@
 #include <stdlib.h> 
 #include <string.h> 
 #include <stdio.h> 
+#include <fcntl.h> 
 
 #include "utilities.h" 
 
 int sys_call_helper(tok_t arg[]) {
+  int default_input = -1, default_output = -1;
+  io_redirect(arg, &default_input, &default_output);
   execv(arg[0], arg);
   char *path_var = getenv("PATH");
   char *path = strtok(path_var, ":");
@@ -19,5 +22,54 @@ int sys_call_helper(tok_t arg[]) {
     free(cmd);
     path = strtok(NULL, ":");
   } 
+  io_restore(default_input, default_output);
   return -1;
+}
+
+int io_redirect(tok_t arg[], int *default_input, int *default_output) {
+  *default_input = dup(STDIN_FILENO);
+  *default_output = dup(STDOUT_FILENO);
+  int input = -1, output = -1, index = 0;
+  for (int i = 0; arg[i]; ++i) {
+    if (!strcmp(arg[i], "<")) {
+      if ((input = open(arg[++i], O_RDONLY)) < 0) {
+        file_close(input, output);
+        return -1; 
+      }
+    } else if (!strcmp(arg[i], ">")) { 
+      if ((output = open(arg[++i], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR)) < 0) {
+        file_close(input, output);
+        return -1; 
+      }
+    } else { 
+      arg[index++] = arg[i]; 
+    } 
+  }
+  arg[index] = NULL;
+  if (input >= 0) {
+    dup2(input, STDIN_FILENO); 
+    close(input);
+  }
+  if (output >= 0) {
+    dup2(output, STDOUT_FILENO); 
+    close(output);
+  }
+  return 0;
+} 
+
+void io_restore(int default_input, int default_output) {
+  dup2(default_input, STDIN_FILENO);
+  dup2(default_output, STDOUT_FILENO);
+  close(default_input);
+  close(default_output);
+}
+
+void file_close(int input, int output) {
+  if (input >= 0) {
+    close(input);
+  } 
+  if (output >= 0) {
+    close(output);
+  }
+  perror("Fail opening a file");
 }
--- personal/hw1/utilities.h
+++ personal/hw1/utilities.h
@@ -4,5 +4,8 @@
 #include "parse.h" 
 
 int sys_call_helper(tok_t arg[]);
+int io_redirect(tok_t arg[], int *default_input, int *default_output); 
+void io_restore(int default_input, int default_output);
+void file_close(int input, int output);
 
 #endif /* UTILITIES_H */
